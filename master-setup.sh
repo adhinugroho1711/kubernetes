fb71c8407f01ae29a583bd9c9f800fec31114ba2
Adhi Nugroho <genva@Adhis-MacBook-Pro.local>
2019-08-24 15:58:36 +0700

    kubernetes-master-slave-setup

---
+++ master-setup.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update && apt-get install docker-ce=18.06.2~ce~3-0~ubuntu -y
+apt-get update && sudo apt-get install -y apt-transport-https
+sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+apt-get update && sudo apt-get install kubelet kubeadm kubectl -y
+sudo kubeadm config images pull
+kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address=192.168.64.7
+
+sleep 30
+
+mkdir -p $HOME/.kube
+cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+chown $(id -u):$(id -g) $HOME/.kube/config
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/62e44c867a2846fefb68bd5f178daf4da3095ccb/Documentation/kube-flannel.yml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v1.10.1/src/deploy/recommended/kubernetes-dashboard.yaml
+echo '  type: NodePort' >> kubernetes-dashboard.yaml
+kubectl create -f kubernetes-dashboard.yaml
+
+sudo bash -c 'cat << EOF > admin-user.yaml
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: admin-user
+  namespace: kube-system
+EOF'
+ 
+kubectl create -f admin-user.yaml
+ 
+# Create an admin role that will be needed in order to access the Kubernetes Dashboard
+sudo bash -c 'cat << EOF > role-binding.yaml
+apiVersion: rbac.authorization.k8s.io/v1beta1
+kind: ClusterRoleBinding
+metadata:
+  name: admin-user
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: cluster-admin
+subjects:
+- kind: ServiceAccount
+  name: admin-user
+  namespace: kube-system
+EOF'
+ 
+kubectl create -f role-binding.yaml
+
+# This command will print the port exposed by the Kubernetes dashboard service. We need to connect to the floating IP:PORT later
+kubectl -n kube-system get service kubernetes-dashboard
+ 
+ 
+# This command will print a token that can be used to authenticate in the Kubernetes dashboard
+kubectl -n kube-system describe secret $(kubectl -n kube-system get secret | grep admin-user | awk '{print $1}') | grep "token:"
---
+++ slave-setup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update && apt-get install docker-ce=18.06.2~ce~3-0~ubuntu -y
+apt-get update && sudo apt-get install -y apt-transport-https
+sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
+apt-get update && sudo apt-get install kubelet kubeadm kubectl -y
