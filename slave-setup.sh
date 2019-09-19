98dcf159fac2045e4d6b0b59bf097c1b350a3fb6
P Adhi Nugroho <phenom1711@gmail.com>
2019-09-19 21:03:12 +0700

    Update slave-setup.sh
    change kubernetes version

--- slave-setup.sh
+++ slave-setup.sh
@@ -5,4 +5,4 @@ apt-get update && apt-get install docker-ce=18.06.2~ce~3-0~ubuntu -y
 apt-get update && sudo apt-get install -y apt-transport-https
 sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
-apt-get update && sudo apt-get install kubelet kubeadm kubectl -y
+apt-get update && sudo apt-get install -qy kubelet=1.15.4-00 kubeadm=1.15.4-00 kubectl=1.15.4-00 -y
