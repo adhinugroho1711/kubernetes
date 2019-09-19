38ca254d54b12b644c37bf4807ba626899640b98
P Adhi Nugroho <phenom1711@gmail.com>
2019-09-19 20:51:41 +0700

    Update master-setup.sh
    adding kubernetes specific version

--- master-setup.sh
+++ master-setup.sh
@@ -5,7 +5,7 @@ apt-get update && apt-get install docker-ce=18.06.2~ce~3-0~ubuntu -y
 apt-get update && sudo apt-get install -y apt-transport-https
 sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
-apt-get update && sudo apt-get install kubelet kubeadm kubectl -y
+apt-get update && sudo apt-get install -qy kubelet=1.15.4-00 kubeadm=1.15.4-00 kubectl=1.15.4-00 -y
 sudo kubeadm config images pull
 sudo kubeadm init --pod-network-cidr=10.244.0.0/16 --apiserver-advertise-address=192.168.64.7
 
