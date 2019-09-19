c595984cd8966a7f60b67428f0f6fc7d3c9754ab
P Adhi Nugroho <phenom1711@gmail.com>
2019-09-19 17:43:04 +0700

    Update master-setup.sh
    change link for flannel

--- master-setup.sh
+++ master-setup.sh
@@ -14,7 +14,7 @@ sleep 30
 mkdir -p $HOME/.kube
 cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
-kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/62e44c867a2846fefb68bd5f178daf4da3095ccb/Documentation/kube-flannel.yml
+kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/v0.9.1/Documentation/kube-flannel.yml
 kubectl apply -f https://raw.githubusercontent.com/phenom1711/kubernetes/master/kubernetes-dashboard.yaml
 echo '  type: NodePort' >> kubernetes-dashboard.yaml
 
