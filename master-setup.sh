e044f889f6870765d3435880aa559224e95e0df1
P Adhi Nugroho <phenom1711@gmail.com>
2019-09-24 05:21:32 +0700

    Update master-setup.sh

--- master-setup.sh
+++ master-setup.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+swapoff -a
 apt-get update && apt-get install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
